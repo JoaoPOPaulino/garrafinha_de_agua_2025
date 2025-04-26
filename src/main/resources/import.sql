c5a9f89961b19e9047888277bd442152bf8bc0b5
paulagomes@unitins.br <paulagomes@unitins.br>
2025-04-26 19:48:24 -0300

    Messagem Teste

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO marca (nome, cnpj) VALUES ('Marca 2', '12345678000196');
 INSERT INTO marca (nome, cnpj) VALUES ('Marca 3', '12345678000197');
 INSERT INTO marca (nome, cnpj) VALUES ('Marca 4', '12345678000198');
 INSERT INTO marca (nome, cnpj) VALUES ('Marca 5', '12345678000199');
+INSERT INTO marca (nome, cnpj) VALUES ('Marca 6', '10000000000000');
